0f3d34c7da4a9464e34d9810c8aca3445b8d40d4
zsevic <zsevic95@gmail.com>
2020-05-04 23:15:37 +0200

    Set team members min and max length

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,9 @@ const REGISTRATION_FORM = '#registration-form';
 
 $(REGISTRATION_FORM).on('submit', event => {
   event.preventDefault();
-  const formData = $(REGISTRATION_FORM).serialize();
+  const formData = $(`${REGISTRATION_FORM} :input`)
+    .filter((_, element) => $(element).val() !== '')
+    .serialize();
   $.ajax('/teams', {
     data: formData,
     type: 'POST',
--- src/modules/team/dto/create-team.dto.ts
+++ src/modules/team/dto/create-team.dto.ts
@@ -1,6 +1,15 @@
 import { Type } from 'class-transformer';
-import { IsNotEmpty, ValidateNested } from 'class-validator';
+import {
+  IsNotEmpty,
+  ValidateNested,
+  ArrayMinSize,
+  ArrayMaxSize,
+} from 'class-validator';
 import { CreateMemberDto } from 'modules/member/dto';
+import {
+  MEMBERS_MAX_LENGTH,
+  MEMBERS_MIN_LENGTH,
+} from 'modules/member/member.constants';
 
 export class CreateTeamDto {
   @IsNotEmpty()
@@ -8,6 +17,8 @@ export class CreateTeamDto {
 
   @IsNotEmpty()
   @ValidateNested({ each: true })
+  @ArrayMinSize(MEMBERS_MIN_LENGTH)
+  @ArrayMaxSize(MEMBERS_MAX_LENGTH)
   @Type(() => CreateMemberDto)
   members: CreateMemberDto[];
 }

610f659cb8a34b2cce79be7fa7de5d49db61fbdf
zsevic <zsevic95@gmail.com>
2020-05-10 02:15:57 +0200

    Add rate limit middleware

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
+import * as rateLimit from 'express-rate-limit';
 import * as helmet from 'helmet';
 import { AppModule } from 'modules/app/app.module';
 import { setupApiDocs } from 'common/config/api-docs';
+import { RATE_LIMIT_REQUESTS, RATE_LIMIT_TIME } from 'common/config/rate-limit';
 import { setupTemplateEngine } from 'common/config/template-engine';
 import { AllExceptionsFilter } from 'common/filters';
 import { loggerMiddleware } from 'common/middlewares';
@@ -17,6 +19,7 @@ async function bootstrap(): Promise<void> {
   const logger = new Logger(bootstrap.name);
   const configService = app.get('configService');
 
+  app.set('trust proxy', 1); // used for rate limiter
   app.setBaseViewsDir(join(__dirname, '../..', 'views'));
   setupTemplateEngine(__dirname);
   app.setViewEngine('hbs');
@@ -24,6 +27,12 @@ async function bootstrap(): Promise<void> {
   app.use(cookieParser());
   app.use(helmet());
   app.use(loggerMiddleware);
+  app.use(
+    rateLimit({
+      windowMs: RATE_LIMIT_TIME,
+      max: RATE_LIMIT_REQUESTS,
+    }),
+  );
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(
     new CustomValidationPipe({

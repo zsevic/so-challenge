a7078ca4bae2c87ebf6f6ab8f87703d1d5a4f2ff
zsevic <zsevic95@gmail.com>
2020-05-25 22:52:54 +0200

    Update API docs

--- src/common/config/api-docs/constants.ts
+++ src/common/config/api-docs/constants.ts
@@ -1,4 +1,4 @@
-export const SWAGGER_API_NAME = 'so-challenge backend';
+export const SWAGGER_API_NAME = 'so-challenge back-end';
 export const SWAGGER_API_DESCRIPTION = 'so-challenge API documentation';
 export const SWAGGER_API_CURRENT_VERSION = '0.0.1';
 export const SWAGGER_API_ROOT = 'api-docs';

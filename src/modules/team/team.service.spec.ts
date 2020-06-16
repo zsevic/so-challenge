8ec2e4f2ab55a9a9801b9bacd5ea02a6fc54ab3a
zsevic <zsevic95@gmail.com>
2020-06-16 03:05:40 +0200

    Fix tests

--- src/modules/team/team.service.spec.ts
+++ src/modules/team/team.service.spec.ts
@@ -83,49 +83,59 @@ describe('TeamService', () => {
   });
 
   it('should return an array of teams', async () => {
-    const teamList = [
-      {
-        id: '370b670e-6d78-44de-be26-3d3af4d02faf',
-        name: 'team1',
-        score: 0,
-        created_at: '2020-05-20T22:25:22.000Z',
-        updated_at: '2020-05-21T00:48:11.000Z',
-        members: [
-          {
-            id: 'c51555dd-4e44-4259-a89a-f5f82b4b599e',
-            stackoverflow_id: 335384,
-            team_id: '370b670e-6d78-44de-be26-3d3af4d02faf',
-            link: '',
-            name: 'Andy LifeBrixx',
-            score: 0,
-            created_at: '2020-05-20T22:25:22.000Z',
-            updated_at: '2020-05-20T22:25:22.000Z',
-          },
-          {
-            id: '55be4f25-36d0-447b-8e3e-c297ac647026',
-            stackoverflow_id: 235254,
-            team_id: '370b670e-6d78-44de-be26-3d3af4d02faf',
-            link: '',
-            name: 'user235254',
-            score: 0,
-            created_at: '2020-05-20T22:25:22.000Z',
-            updated_at: '2020-05-20T22:25:22.000Z',
-          },
-          {
-            id: '613dd7e4-50a1-4f27-a0a7-f572ac27e268',
-            stackoverflow_id: 235384,
-            team_id: '370b670e-6d78-44de-be26-3d3af4d02faf',
-            link: '',
-            name: 'user235384',
-            score: 0,
-            created_at: '2020-05-20T22:25:22.000Z',
-            updated_at: '2020-05-20T22:25:22.000Z',
-          },
-        ],
-      },
-    ];
+    const teamList = {
+      totalCount: 1,
+      limit: 1,
+      page: 1,
+      data: [
+        {
+          id: '370b670e-6d78-44de-be26-3d3af4d02faf',
+          name: 'team1',
+          score: 0,
+          created_at: '2020-05-20T22:25:22.000Z',
+          updated_at: '2020-05-21T00:48:11.000Z',
+          members: [
+            {
+              id: 'c51555dd-4e44-4259-a89a-f5f82b4b599e',
+              stackoverflow_id: 335384,
+              team_id: '370b670e-6d78-44de-be26-3d3af4d02faf',
+              link: '',
+              name: 'Andy LifeBrixx',
+              score: 0,
+              created_at: '2020-05-20T22:25:22.000Z',
+              updated_at: '2020-05-20T22:25:22.000Z',
+            },
+            {
+              id: '55be4f25-36d0-447b-8e3e-c297ac647026',
+              stackoverflow_id: 235254,
+              team_id: '370b670e-6d78-44de-be26-3d3af4d02faf',
+              link: '',
+              name: 'user235254',
+              score: 0,
+              created_at: '2020-05-20T22:25:22.000Z',
+              updated_at: '2020-05-20T22:25:22.000Z',
+            },
+            {
+              id: '613dd7e4-50a1-4f27-a0a7-f572ac27e268',
+              stackoverflow_id: 235384,
+              team_id: '370b670e-6d78-44de-be26-3d3af4d02faf',
+              link: '',
+              name: 'user235384',
+              score: 0,
+              created_at: '2020-05-20T22:25:22.000Z',
+              updated_at: '2020-05-20T22:25:22.000Z',
+            },
+          ],
+        },
+      ],
+    };
     jest.spyOn(repository, 'getTeamList').mockResolvedValue(teamList);
 
-    expect(await service.getTeamList()).toMatchObject(teamList);
+    expect(
+      await service.getTeamList({
+        limit: 1,
+        page: 1,
+      }),
+    ).toMatchObject(teamList);
   });
 });
